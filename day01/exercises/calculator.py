fa00d4d8da1804156991aa113d500461afd36c63
basilmb <basilmb507@gmail.com>
2026-06-17 20:21:07 +0530

    fix code formatting and indentation

--- day01/exercises/calculator.py
+++ day01/exercises/calculator.py
@@ -1,9 +1,12 @@
 def add(num1, num2):
    return num1 + num2
+
 def subtract(num1, num2):
    return num1 - num2
+
 def multiply(num1, num2):
    return num1 * num2
+
 def divide(num1, num2):
    if num2 == 0:
        return "Error: Cannot divide by zero"
@@ -54,6 +57,3 @@ def calculator():
         
 if __name__ == "__main__":
     calculator()
-        
-            
-            
